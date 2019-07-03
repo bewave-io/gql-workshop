cf4980d7f0fa171bcf736ed0536010f4369ee249
emilio.aguirre <emilio.aguirre@bewave.io>
2019-07-03 15:57:13 -0400

    Adding mutations

--- schema.py
+++ schema.py
@@ -1,18 +1,39 @@
-from graphene import ObjectType, String, Schema, List, Int, Field
+from graphene import ObjectType, String, Schema, List, Int, Field, Boolean, Mutation
+from uuid import uuid4
 
 
 def resolve_full_name(person, info):
     return f"{person.first_name} {person.last_name}"
 
 
+class CreatePerson(Mutation):
+    class Arguments:
+        first = String()
+        last = String()
+
+    ok = Boolean()
+    person = Field(lambda: Person)
+
+    def mutate(root, info, first, last):
+        person = Person(first=first, last=last)
+        ok = True
+        return CreatePerson(person=person, ok=ok)
+
+
+class MyMutations(ObjectType):
+    create_person = CreatePerson.Field()
+
+
 class Person(ObjectType):
     first_name = String()
     last_name = String()
+    id = String()
     full_name = String(resolver=resolve_full_name)
 
     def __init__(self, first, last):
         self.first_name = first
         self.last_name = last
+        self.id = str(uuid4())
 
 
 class QueryData(ObjectType):
@@ -41,4 +62,4 @@ class QueryData(ObjectType):
         return Person("Jo", "Bin")
 
 
-schema = Schema(query=QueryData)
+schema = Schema(query=QueryData, mutation=MyMutations)

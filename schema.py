3f2a555f4390e26bd5eded191399c95ba06029be
emilio.aguirre <emilio.aguirre@bewave.io>
2019-07-03 15:25:36 -0400

    Adding person object

--- schema.py
+++ schema.py
@@ -1,4 +1,18 @@
-from graphene import ObjectType, String, Schema, List, Int
+from graphene import ObjectType, String, Schema, List, Int, Field
+
+
+def resolve_full_name(person, info):
+    return f"{person.first_name} {person.last_name}"
+
+
+class Person(ObjectType):
+    first_name = String()
+    last_name = String()
+    full_name = String(resolver=resolve_full_name)
+
+    def __init__(self, first, last):
+        self.first_name = first
+        self.last_name = last
 
 
 class QueryData(ObjectType):
@@ -7,6 +21,7 @@ class QueryData(ObjectType):
     goodbye = String()
     info = String()
     stuff = List(String, idx=Int(default_value=0))
+    person = Field(Person)
 
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (name) for the Field and returns data for the query Response
@@ -21,6 +36,9 @@ class QueryData(ObjectType):
 
     def resolve_stuff(root, info, idx):
         return [["1", "2", "3"][idx]]
+
+    def resolve_person(root, info):
+        return Person("Jo", "Bin")
 
 
 schema = Schema(query=QueryData)
